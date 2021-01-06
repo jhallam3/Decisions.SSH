a408a4bf64d2d27891daa5fa4d22934b67650b5a
Jon <Jon@WIN-B2L934R07O3>
2021-01-06 08:59:25 +0000

    SSH with SSH.net

---
+++ DecisionsSSH/Decisions.SSH/SSHSteps.cs
@@ -0,0 +1,59 @@
+﻿using DecisionsFramework.Design.Flow;
+using Renci.SshNet;
+using System;
+
+namespace Decisions.SSH
+{
+    [AutoRegisterMethodsOnClass(true, "Integration/SSH")]
+    public class SSHSteps
+    {
+        public bool TryConnect(ConnectionInfo connectionInfo)
+        {
+            try
+            {
+                
+                using (var client = new SshClient(connectionInfo))
+                {
+                    client.Connect();
+                    return client.IsConnected;
+                }
+            }
+            catch (Exception ex2)
+            {
+
+                return false;
+            }
+        }
+
+
+        public ConnectionInfo CreateConnectionInfo(string host, string username, string password)
+        {
+            return new ConnectionInfo(host, username,
+                                              new PasswordAuthenticationMethod(username, password));
+        }
+       
+
+
+
+        public string RunCommand(string host, string username, string password, string command)
+        {
+            try
+            {
+                using (var client = new SshClient(new ConnectionInfo(host, username, new PasswordAuthenticationMethod(username, password))))
+                {
+                    client.Connect();
+                    client.RunCommand(command);
+                    return "done";
+                }
+            }
+            catch (Exception ex2)
+            {
+
+                throw;
+            }
+        }
+
+   
+
+    }
+}
